04f27a15bc2eb2b54cc84d79bfe6db931c6ff451
ArturFBAlves <arturfbalves@gmail.com>
2025-11-17 00:18:07 -0300

    cadastro e login linkado com o backend

---
+++ JAVA/src/main/java/springBoot/FarmaShopApplication.java
@@ -0,0 +1,11 @@
+package springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FarmaShopApplication.class, args);
+    }
+}
